59bc0ecae8220060a3adab3f73b61d6b114f83fd
sunsetdee <andiilii@yahoo.com>
2021-06-16 20:24:29 -0700

    finish delete function

--- models/skill.js
+++ models/skill.js
@@ -10,7 +10,13 @@ const skills = [
     getAll,
     getOne,
     create,
+    deleteOne,
   };
+
+  function deleteOne(id) {
+    const idx = skills.findIndex(todo => skills.id === parseInt(id));
+    skills.splice(idx, 1);
+  }
 
   function create(skill) {
     skill.id= Date.now() % 1000000;
--- routes/skills.js
+++ routes/skills.js
@@ -8,6 +8,7 @@ router.get('/', skillsCtrl.index);
 router.get('/new', skillsCtrl.new);
 router.get('/:id', skillsCtrl.show);
 router.post('/', skillsCtrl.create);
+router.delete('/:id', skillsCtrl.delete);
 
 
 
